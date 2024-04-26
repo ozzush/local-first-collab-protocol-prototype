3f01913dc8744b7b07ba47648aed047183df1cdb
veron <veronika@telamon.ru>
2024-04-26 15:00:19 +0200

    fix protocol so that the log is strictly ordered

--- common/src/main/kotlin/common/LogModel.kt
+++ common/src/main/kotlin/common/LogModel.kt
@@ -3,17 +3,22 @@ package common
 class LogModel {
     private val log = mutableListOf(UpdateDescriptor("server", 0, 1))
 
+    fun deepCopy() = LogModel().also {
+        it.log.clear()
+        it += log
+    }
+
     fun add(updateDescriptor: UpdateDescriptor) {
         log.add(updateDescriptor)
-        println("Log updated: $log")
     }
 
     operator fun plusAssign(other: List<UpdateDescriptor>) {
         log += other
-        println("Log updated: $log")
     }
 
     fun last() = log.last()
+
+    fun data() = log.toList()
 
     fun lastFromAuthor(author: String) = log.findLast { it.author == author }
 }
